2094fdbfcf3699e05356d1e5eb89a4fb680017fa
jaynabonne <devnull@localhost>
2014-12-20 10:31:22 +0000

    Refactored

--- src/tokenize.js
+++ src/tokenize.js
@@ -1,45 +1,46 @@
-//var tokenize = (function() {
-//})();
-
-function isAToken(part) {
-    return part.charAt(0) === ".";
-}
-
-function extractToken(parts, index) {
-    return parts[index].slice(1);
-}
-
-function extractValue(parts, index, last_index) {
-    return parts.slice(index, last_index).join(" ").trim();
-}
-
-function extractTokenPair(parts, index, last_index) {
-    return {
-        token: extractToken(parts, index),
-        value: extractValue(parts, index+1, last_index)
-    };
-}
-
-function findNextToken(index, parts) {
-    while (index < parts.length && !isAToken(parts[index])) {
-        ++index;
+var tokenize = (function() {
+    "use strict";
+    
+    function isAToken(part) {
+        return part.charAt(0) === ".";
     }
-    return index;
-}
 
-function tokenize(input) {
-    if (input === "") {
-        return [];
+    function extractToken(parts, index) {
+        return parts[index].slice(1);
     }
-    var parts = input.split(/[\s,]/);
-    var result = [];
 
-    var index = findNextToken(0, parts);
-    while (index < parts.length) {
-        var last_index = findNextToken(index+1, parts);
-
-        result.push(extractTokenPair(parts, index, last_index));
-        index = last_index;
+    function extractValue(parts, index, last_index) {
+        return parts.slice(index, last_index).join(" ").trim();
     }
-    return result;
-}
+
+    function extractTokenPair(parts, index, last_index) {
+        return {
+            token: extractToken(parts, index),
+            value: extractValue(parts, index+1, last_index)
+        };
+    }
+
+    function findNextToken(index, parts) {
+        while (index < parts.length && !isAToken(parts[index])) {
+            ++index;
+        }
+        return index;
+    }
+
+    return function(input) {
+        if (input === "") {
+            return [];
+        }
+        var parts = input.split(/[\s,]/);
+        var result = [];
+
+        var index = findNextToken(0, parts);
+        while (index < parts.length) {
+            var last_index = findNextToken(index+1, parts);
+
+            result.push(extractTokenPair(parts, index, last_index));
+            index = last_index;
+        }
+        return result;
+    }
+})();
