7172a581a7d9d238f5b53803ab51e4f9732516d8
jaynabonne <devnull@localhost>
2014-12-22 20:28:40 +0000

    Refactored: got rid of handlers struct.

--- src/rif_parse.js
+++ src/rif_parse.js
@@ -7,29 +7,26 @@ rifParse = (function () {
         this.index = 0;
     };
 
-    Parser.prototype.handlers = {
-        object: function () {
-            var value = this.tokens[this.index].value;
-            var rif = this.rif;
-            rif.objects = rif.objects || {};
-            rif.objects[value] = {};
-            this.index++;
-        },
-        responses: function () {
-            var value = this.tokens[this.index].value;
-            var rif = this.rif;
-            rif.responses = rif.responses || {};
-            rif.responses[value] = [];
-            this.index += 2;
-        }
+    Parser.prototype.object = function () {
+        var value = this.tokens[this.index].value;
+        var rif = this.rif;
+        rif.objects = rif.objects || {};
+        rif.objects[value] = {};
+        this.index++;
+    };
+    Parser.prototype.responses = function () {
+        var value = this.tokens[this.index].value;
+        var rif = this.rif;
+        rif.responses = rif.responses || {};
+        rif.responses[value] = [];
+        this.index += 2;
     };
 
     Parser.prototype.parse = function() {
         while (this.index < this.tokens.length) {
             var token = this.tokens[this.index].token;
-            if (this.handlers[token]) {
-                var func = (this.handlers[token]).bind(this);
-                func();
+            if (this[token]) {
+                this[token]();
             } else {
                 console.log("no handler for token " + token);
                 break;
