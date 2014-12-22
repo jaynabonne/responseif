4de0e63d3c69f0739b1ecfd4645e853afde6bbf4
jaynabonne <devnull@localhost>
2014-12-22 01:13:01 +0000

    Added initial responses processing test.

--- src/rif_parse.js
+++ src/rif_parse.js
@@ -2,8 +2,14 @@ var rifParse = (function() {
     return function(tokens) {
         var rif = {};
         if (tokens.length > 0) {
-            rif.objects = {};
-            rif.objects[tokens[0].value] = {};
+            var token = tokens[0];
+            if (token.token === "object") {
+                rif.objects = {};
+                rif.objects[tokens[0].value] = {};
+            } else if (token.token === "responses") {
+                rif.responses = {};
+                rif.responses[tokens[0].value] = {};
+            }
         }
         return rif;
     };
--- test/rif_parse_spec.js
+++ test/rif_parse_spec.js
@@ -3,15 +3,16 @@ describe("rifParse", function () {
         expect(rifParse([])).toEqual({});
     });
     it("should parse an empty object", function () {
+        var rif = rifParse( [ {token: "object", value: "anObject"} ] );
+        expect(rif.objects).toEqual( { anObject: {} } );
+    });
+    it("should parse an empty responses list", function () {
         var rif = rifParse(
             [
-                {token: "object", value: "anObject"}
+                {token: "responses", value: "someObject"},
+                {token: "end", value: ""}
             ]
         );
-        expect(rif.objects).toEqual(
-            {
-                anObject: {}
-            }
-        );
+        expect(rif.responses).toEqual( { someObject: {} } );
     });
 });
