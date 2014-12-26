ab640dfd820419a8e4c005e986c9c1b6f99e1466
jaynabonne <devnull@localhost>
2014-12-26 01:03:32 +0000

    Refactored.

--- src/rif_expand.js
+++ src/rif_expand.js
@@ -6,23 +6,25 @@ var rifExpand = (function () {
         this.definitions = {};
         this.index = 0;
     };
+    Expander.prototype.expandNext = function() {
+        var token_pair = this.tokens[this.index];
+        var token = token_pair.token;
+        if (token === "define") {
+            var definition = [];
+            var start = ++this.index;
+            while (this.index < this.tokens.length && this.tokens[this.index].token != "enddef")
+                ++this.index;
+            this.definitions[token_pair.value] = this.tokens.slice(start, this.index);
+            return;
+        } else if (this.definitions[token]) {
+            this.new_tokens = this.new_tokens.concat(this.definitions[token]);
+            return;
+        }
+        this.new_tokens.push(token_pair);
+    };
     Expander.prototype.expand = function() {
-        var tokens = this.tokens;
-        for (this.index = 0; this.index < tokens.length; ++this.index) {
-            var token_pair = tokens[this.index];
-            var token = token_pair.token;
-            if (token === "define") {
-                var definition = [];
-                var start = ++this.index;
-                while (this.index < this.tokens.length && this.tokens[this.index].token != "enddef")
-                    ++this.index;
-                this.definitions[token_pair.value] = this.tokens.slice(start, this.index);
-                continue;
-            } else if (this.definitions[token]) {
-                this.new_tokens = this.new_tokens.concat(this.definitions[token]);
-                continue;
-            }
-            this.new_tokens.push(token_pair);
+        for (this.index = 0; this.index < this.tokens.length; ++this.index) {
+            this.expandNext();
         }
         return this.new_tokens;
     };
