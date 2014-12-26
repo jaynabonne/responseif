ab21f1ba82910825c04714a0b6cf9268e3ce8846
jaynabonne <devnull@localhost>
2014-12-26 12:52:42 +0000

    Refactored.

--- src/rif_expand.js
+++ src/rif_expand.js
@@ -1,32 +1,61 @@
 var rifExpand = (function () {
     "use strict";
-    var Expander = function(tokens) {
+
+    var Iterator = function(tokens) {
         this.tokens = tokens;
+        this.index = 0;
+        this.length = tokens.length;
+    };
+
+    Iterator.prototype.done = function() {
+        return this.index === this.length;
+    };
+
+    Iterator.prototype.next = function() {
+        return ++this.index;
+    };
+
+    Iterator.prototype.get = function() {
+        return this.tokens[this.index];
+    };
+
+    var Expander = function(tokens) {
+        this.iterator = new Iterator(tokens);
         this.new_tokens = [];
         this.definitions = {};
     };
 
-    Expander.prototype.processDefinition = function(id) {
-        var start = ++this.index;
-        while (this.index < this.tokens.length && this.tokens[this.index].token != "enddef")
-            ++this.index;
-        this.definitions[id] = this.tokens.slice(start, this.index);
-    }
+    Expander.prototype.createDefinition = function(id) {
+        var definition = [];
+        while (!this.iterator.done() && this.iterator.get().token != "enddef") {
+            definition.push(this.iterator.get());
+            this.iterator.next();
+        }
+        this.definitions[id] = definition;
+    };
+
+    Expander.prototype.useDefinition = function(token) {
+        var expanded_tokens = this.definitions[token];
+        this.new_tokens = this.new_tokens.concat(expanded_tokens);
+    };
 
     Expander.prototype.expandNext = function() {
-        var token_pair = this.tokens[this.index];
+        var token_pair = this.iterator.get();
         var token = token_pair.token;
         if (token === "define") {
-            this.processDefinition(token_pair.value);
+            this.iterator.next();
+            this.createDefinition(token_pair.value);
         } else if (this.definitions[token]) {
-            this.new_tokens = this.new_tokens.concat(this.definitions[token]);
+            this.useDefinition(token);
         } else {
             this.new_tokens.push(token_pair);
         }
     };
+
     Expander.prototype.expand = function() {
-        for (this.index = 0; this.index < this.tokens.length; ++this.index) {
+        while(!this.iterator.done()) {
             this.expandNext();
+            this.iterator.next();
         }
         return this.new_tokens;
     };
