1348107589f087b3812f123b19716a76ad5eaf18
jaynabonne <devnull@localhost>
2014-12-26 01:12:04 +0000

    Refactored.

--- src/rif_expand.js
+++ src/rif_expand.js
@@ -4,23 +4,25 @@ var rifExpand = (function () {
         this.tokens = tokens;
         this.new_tokens = [];
         this.definitions = {};
-        this.index = 0;
     };
+
+    Expander.prototype.processDefinition = function(id) {
+        var start = ++this.index;
+        while (this.index < this.tokens.length && this.tokens[this.index].token != "enddef")
+            ++this.index;
+        this.definitions[id] = this.tokens.slice(start, this.index);
+    }
+
     Expander.prototype.expandNext = function() {
         var token_pair = this.tokens[this.index];
         var token = token_pair.token;
         if (token === "define") {
-            var definition = [];
-            var start = ++this.index;
-            while (this.index < this.tokens.length && this.tokens[this.index].token != "enddef")
-                ++this.index;
-            this.definitions[token_pair.value] = this.tokens.slice(start, this.index);
-            return;
+            this.processDefinition(token_pair.value);
         } else if (this.definitions[token]) {
             this.new_tokens = this.new_tokens.concat(this.definitions[token]);
-            return;
+        } else {
+            this.new_tokens.push(token_pair);
         }
-        this.new_tokens.push(token_pair);
     };
     Expander.prototype.expand = function() {
         for (this.index = 0; this.index < this.tokens.length; ++this.index) {
