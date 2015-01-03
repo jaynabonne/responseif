aebb9805d9ffa4239f3fd13628f918e2a99cfd1e
jaynabonne <devnull@localhost>
2015-01-03 20:49:28 +0000

    Added test for scrollToEnd.

--- test/InteractSpec.js
+++ test/InteractSpec.js
@@ -19,5 +19,10 @@ describe("Interact", function () {
             interact.say("This is some text");
             expect(appendSpy).toHaveBeenCalledWith("formattedText");
         });
+        it("should scroll to the end of the text", function() {
+            dom.scrollToEnd = jasmine.createSpy("scrollToEnd");
+            interact.say("This is some text");
+            expect(dom.scrollToEnd).toHaveBeenCalled();
+        });
     });
 });
