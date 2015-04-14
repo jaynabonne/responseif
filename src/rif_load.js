1b5f0b66c57a48cd83a7881a05cb8eb644fc8728
jaynabonne <devnull@localhost>
2015-04-14 20:13:08 +0100

    Refactored

--- src/rif_load.js
+++ src/rif_load.js
@@ -4,17 +4,25 @@ var rifLoad = (function() {
         this.load_file = load_file;
     };
 
+    function addIncludes(completion) {
+        while (this.index < this.tokens.length) {
+            var token_pair = this.tokens[this.index];
+            if (token_pair.token === 'include') {
+                console.info(this);
+                this.load_file(token_pair.value, function(data) {
+                });
+            }
+            ++this.index;
+        }
+        completion(this.tokens);
+    }
+
     type.prototype.loadTokens = function(name, completion) {
         var self = this;
         self.load_file(name, function (data) {
-            var tokens = rifTokenize(data);
-            $.each(tokens, function(index, token_pair) {
-                if (token_pair.token === 'include') {
-                    self.load_file(token_pair.value, function(data) {
-                    });
-                }
-            });
-            completion(tokens);
+            self.tokens = rifTokenize(data);
+            self.index = 0;
+            addIncludes.call(self, completion);
         });
     };
 
