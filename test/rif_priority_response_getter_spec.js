3ee6973011b3930e33130a07512bc5c3181f3a17
Jay Nabonne <jaynabonne@gmail.com>
2016-02-27 20:20:17 +0000

    Moved priority response getter tests to separate file.

---
+++ test/rif_priority_response_getter_spec.js
@@ -0,0 +1,29 @@
+define(['rif/priority_response_getter'], function(RifPriorityResponseGetter) {
+    "use strict";
+
+    describe("priority response getter", function () {
+        it("returns an empty list for no input", function () {
+            var responses = new RifPriorityResponseGetter([]).results;
+            expect(responses).toEqual([]);
+        });
+        it("returns a single entry", function () {
+            var response1 = {a: 1, score: 10000};
+            var responses = new RifPriorityResponseGetter([response1]).results;
+            expect(responses).toEqual([response1]);
+        });
+        it("returns the entry with the higher score for two entries", function () {
+            var response1 = {a: 1, score: 10000};
+            var response2 = {a: 2, score: 20000};
+            var responses = new RifPriorityResponseGetter([response1, response2]).results;
+            expect(responses).toEqual([response2]);
+        });
+        it("returns all the entries with the highest score", function () {
+            var response1 = {a: 1, score: 10000};
+            var response2 = {a: 2, score: 20000};
+            var response3 = {a: 3, score: 20000};
+            var response4 = {a: 4, score: 15000};
+            var responses = new RifPriorityResponseGetter([response1, response2, response3, response4]).results;
+            expect(responses).toEqual([response2, response3]);
+        });
+    });
+});
--- test/rif_response_spec.js
+++ test/rif_response_spec.js
@@ -92,31 +92,6 @@ describe("RifResponse", function () {
             expect(candidates).toEqual([]);
         });
     });
-    describe("getPriorityResponses", function () {
-        it("returns an empty list for no input", function () {
-            var responses = responseLib.getPriorityResponses([]);
-            expect(responses).toEqual([]);
-        });
-        it("returns a single entry", function () {
-            var response1 = {a: 1, score: 10000};
-            var responses = responseLib.getPriorityResponses([response1]);
-            expect(responses).toEqual([response1]);
-        });
-        it("returns the entry with the higher score for two entries", function () {
-            var response1 = {a: 1, score: 10000};
-            var response2 = {a: 2, score: 20000};
-            var responses = responseLib.getPriorityResponses([response1, response2]);
-            expect(responses).toEqual([response2]);
-        });
-        it("returns all the entries with the highest score", function () {
-            var response1 = {a: 1, score: 10000};
-            var response2 = {a: 2, score: 20000};
-            var response3 = {a: 3, score: 20000};
-            var response4 = {a: 4, score: 15000};
-            var responses = responseLib.getPriorityResponses([response1, response2, response3, response4]);
-            expect(responses).toEqual([response2, response3]);
-        });
-    });
     describe("processResponses", function () {
         describe("general", function () {
             it("increments the response run count by one", function () {
