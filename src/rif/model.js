f303884621f4b8e9c9f51b0a9d5e8dfb1d74e4b3
Jay Nabonne <jaynabonne@gmail.com>
2016-04-16 22:21:39 +0100

    More model tests and code.

--- src/rif/model.js
+++ src/rif/model.js
@@ -13,9 +13,29 @@ define([], function() {
     proto.addTopics = function(cluster_id, topics) {
         var cluster = this.clusters[cluster_id] || [];
         $.each(topics, function(index, topic) {
+            for (var i = 0; i < cluster.length; ++i) {
+                var cluster_topic = cluster[i];
+                if (cluster_topic.keyword === topic.keyword) {
+                    cluster_topic.weight = Math.max(cluster_topic.weight, topic.weight);
+                    return;
+                }
+            }
             cluster.push(topic);
         });
         this.clusters[cluster_id] = cluster;
+    };
+    proto.removeTopics = function(cluster_id, topics) {
+        var cluster = this.clusters[cluster_id];
+        if (!cluster) {
+            return;
+        }
+
+        var keywords = topics.map(function(value) { return value.keyword; });
+        for (var i = cluster.length - 1; i >= 0; i--) {
+            if (keywords.indexOf(cluster[i].keyword) != -1) {
+                cluster.splice(i, 1);
+            }
+        }
     };
     return type;
 });
--- test/rif_model_spec.js
+++ test/rif_model_spec.js
@@ -15,16 +15,26 @@ define(['rif/model'], function(RifModel) {
             model.addTopics('cluster_id', [{keyword: 'atopic', weight: 0.9}]);
             expect(model.getTopics('cluster_id')).toEqual([{keyword: 'atopic', weight: 0.9}]);
         });
-        it('should be able to add and get topics for different clusters', function() {
+        it('should add and get topics for different clusters', function() {
             model.addTopics('cluster_id', [{keyword: 'atopic', weight: 0.9}]);
             model.addTopics('cluster_id2', [{keyword: 'btopic', weight: 0.7}]);
             expect(model.getTopics('cluster_id')).toEqual([{keyword: 'atopic', weight: 0.9}]);
             expect(model.getTopics('cluster_id2')).toEqual([{keyword: 'btopic', weight: 0.7}]);
         });
-        it('should be able to add and get multiple cluster topics', function() {
+        it('should add and get multiple cluster topics', function() {
             model.addTopics('cluster_id', [{keyword: 'atopic', weight: 0.9}]);
             model.addTopics('cluster_id', [{keyword: 'btopic', weight: 0.6}]);
             expect(model.getTopics('cluster_id')).toEqual([{keyword: 'atopic', weight: 0.9}, {keyword: 'btopic', weight: 0.6}]);
+        });
+        it('should delete existing topics', function() {
+            model.addTopics('cluster_id', [{keyword: 'atopic', weight: 0.6},{keyword: 'btopic', weight: 0.9},{keyword: 'ctopic', weight: 0.4}]);
+            model.removeTopics('cluster_id', [{keyword: 'atopic'},{keyword: 'ctopic'}]);
+            expect(model.getTopics('cluster_id')).toEqual([{keyword: 'btopic', weight: 0.9}]);
+        });
+        it('should update an existing topic weight if the new one is greater', function() {
+            model.addTopics('cluster_id', [{keyword: 'atopic', weight: 0.6}]);
+            model.addTopics('cluster_id', [{keyword: 'atopic', weight: 0.9}]);
+            expect(model.getTopics('cluster_id')).toEqual([{keyword: 'atopic', weight: 0.9}]);
         });
     });
 });
